0c39cbc39cd41431d8ee49ec9ab156092a97e5ed
[Tomasz Woloszynski] <[tomekwoloszynski@gmail.com]>
2025-05-17 21:57:32 +0800

    Git plugins and tmo.

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "?" },
+			},
+		},
+	},
+	"tpope/vim-fugitive",
+	"sindrets/diffview.nvim",
+}
