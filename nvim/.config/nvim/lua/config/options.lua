c3d2444d47feb6f59ec0c59491a85a8e2678c50f
[Tomasz Woloszynski] <[tomekwoloszynski@gmail.com]>
2025-05-18 20:22:05 +0800

    Alacritty and i3

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -29,3 +29,8 @@ end
 autocmd("CmdlineEnter", "*", "set hlsearch")
 autocmd("CmdlineLeave", "*", "set nohlsearch")
 autocmd("TextYankPost", "*", "silent! lua vim.hl.on_yank()")
+
+vim.cmd.highlight("Normal guibg=none")
+vim.cmd.highlight("NonText guibg=none")
+vim.cmd.highlight("Normal ctermbg=none")
+vim.cmd.highlight("NonText ctermbg=none")
---
+++ nvim/.config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,8 @@
+return {
+	"xiyaowong/transparent.nvim",
+	lazy = false,
+	opts = {},
+	keys = {
+		{ "<leader>T", ":TransparentToggle<cr>", desc = "Toggle transparency" },
+	},
+}
