df5dc7870f092c6378e46ba242a22092a8b226b7
[Tomasz Woloszynski] <[tomekwoloszynski@gmail.com]>
2025-05-25 10:52:29 +0800

    vim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>d", ":lcd %:h<cr>", { desc = "lcd to buffer dir" })
 vim.keymap.set("n", "<leader>t", ":vertical terminal<cr>i", { desc = "Terminal" })
 vim.keymap.set("n", "<leader>w", ":set invwrap<cr>", { desc = "Toggle line wrap" })
-vim.keymap.set("n", "<leader>l", ":Lazy sync<cr>", { desc = "Sync plugins (Lazy)" })
+vim.keymap.set("n", "<leader>u", ":Lazy sync<cr>", { desc = "Sync plugins (Lazy)" })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,5 @@
 vim.opt.number = true
+vim.opt.signcolumn = "yes"
 vim.opt.list = true
 vim.opt.listchars = { tab = ">  ", trail = "-" }
 vim.opt.wrap = false
